735f62c2bd2659b88a6e4e123d910dc0609dd48f
Lukundo Kileha <lkileha@gmail.com>
2021-04-15 20:59:52 +0300

    Merged main

---
+++ door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/KmpUuid.kt
@@ -0,0 +1,10 @@
+package com.ustadmobile.door.util
+
+actual fun randomUuid(): KmpUuid = TODO("Not yet implemented")
+
+actual class KmpUuid actual constructor(mostSigBits: Long, leastSigBits: Long) {
+    actual override fun toString(): String {
+        TODO("Not yet implemented")
+    }
+
+}
