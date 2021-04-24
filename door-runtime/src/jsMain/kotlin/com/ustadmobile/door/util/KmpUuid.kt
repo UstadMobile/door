a561b3251806913b9e15f79a3b70300000b8a666
Lukundo Kileha <lkileha@gmail.com>
2021-04-24 16:14:01 +0300

    Minor update on the uuid function

--- door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/KmpUuid.kt
+++ door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/KmpUuid.kt
@@ -7,10 +7,10 @@ actual fun randomUuid(): KmpUuid = KmpUuid(0L,0L)
 actual class KmpUuid actual constructor(mostSigBits: Long, leastSigBits: Long) {
     actual override fun toString(): String {
         var timeStamp = Date().getTime()
-        return js("'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(mChar) {" +
+        return js("'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(mChar){" +
                 "var mRandom = (timeStamp + Math.random()*16)%16 | 0;" +
                 "timeStamp = Math.floor(timeStamp/16);" +
-                "return (mChar=='x' ? mRandom :(r&0x3|0x8)).toString(16);})") as String
+                "return (mChar=='x' ? mRandom :(mRandom&0x3|0x8)).toString(16);})") as String
     }
 
 }
