64bf78dd933760e52a9af8e2be185d1ed353d430
Lukundo Kileha <lkileha@gmail.com>
2021-04-24 15:48:05 +0300

    Implemented uuid generator

--- door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/KmpUuid.kt
+++ door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/KmpUuid.kt
@@ -1,10 +1,16 @@
 package com.ustadmobile.door.util
 
-actual fun randomUuid(): KmpUuid = TODO("Not yet implemented")
+import kotlin.js.Date
+
+actual fun randomUuid(): KmpUuid = KmpUuid(0L,0L)
 
 actual class KmpUuid actual constructor(mostSigBits: Long, leastSigBits: Long) {
     actual override fun toString(): String {
-        TODO("Not yet implemented")
+        var timeStamp = Date().getTime()
+        return js("'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(mChar) {" +
+                "var mRandom = (timeStamp + Math.random()*16)%16 | 0;" +
+                "timeStamp = Math.floor(timeStamp/16);" +
+                "return (mChar=='x' ? mRandom :(r&0x3|0x8)).toString(16);})") as String
     }
 
 }
