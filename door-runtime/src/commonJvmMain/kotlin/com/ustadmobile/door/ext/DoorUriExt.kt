2e591990355f4da89573516364573034c774595e
Mike Dawson <mike@ustadmobile.com>
2021-06-09 12:11:18 +0400

    Add DoorUri.openInputStream extension function

--- door-runtime/src/commonJvmMain/kotlin/com/ustadmobile/door/ext/DoorUriExt.kt
+++ door-runtime/src/commonJvmMain/kotlin/com/ustadmobile/door/ext/DoorUriExt.kt
@@ -2,9 +2,15 @@ package com.ustadmobile.door.ext
 
 import com.ustadmobile.door.DoorUri
 import java.io.File
+import java.io.InputStream
 
 /**
  * Returns this Uri as a file, if it is a valid file. Otherwise throws IllegalStateException
  */
 expect fun DoorUri.toFile(): File
+
+/**
+ * Open an InputStream for the given Uri.
+ */
+expect fun DoorUri.openInputStream(context: Any): InputStream?
 
