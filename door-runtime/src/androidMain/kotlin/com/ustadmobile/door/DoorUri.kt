d4939841613c7655f948102f3beed20a705ae266
Mike Dawson <mike@ustadmobile.com>
2021-06-13 21:51:55 +0400

    Add DoorUri.getFileName function

--- door-runtime/src/androidMain/kotlin/com/ustadmobile/door/DoorUri.kt
+++ door-runtime/src/androidMain/kotlin/com/ustadmobile/door/DoorUri.kt
@@ -1,11 +1,18 @@
 package com.ustadmobile.door
 
+import android.content.Context
 import android.net.Uri
+import com.ustadmobile.door.ext.getFileName
 
 actual class DoorUri(val uri: Uri) {
+
+    actual suspend fun getFileName(context: Any): String {
+        return (context as Context).contentResolver.getFileName(uri)
+    }
 
     actual companion object {
         actual fun parse(uriString: String) : DoorUri = DoorUri(Uri.parse(uriString))
     }
+
 
 }
--- door-runtime/src/commonMain/kotlin/com/ustadmobile/door/DoorUri.kt
+++ door-runtime/src/commonMain/kotlin/com/ustadmobile/door/DoorUri.kt
@@ -6,6 +6,15 @@ package com.ustadmobile.door
  */
 expect class DoorUri {
 
+    /**
+     * Get the filename of the Uri. On Android this can use the ContentResolver to find a filename that was provided
+     * by a content provider (e.g. an external app). Failing that, this would normally fallback to the substring after
+     * the last slash.
+     *
+     * @param context Context
+     */
+    suspend fun getFileName(context: Any): String
+
     companion object {
         fun parse(uriString: String): DoorUri
     }
