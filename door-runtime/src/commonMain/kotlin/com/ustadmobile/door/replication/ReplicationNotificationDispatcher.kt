f187c2c35033bf259f86299ebb382f43a6ffe7d9
Mike Dawson <mike@ustadmobile.com>
2021-09-19 23:43:02 +0400

    Update ReplicationNotificationDispatcher docs

--- door-runtime/src/commonMain/kotlin/com/ustadmobile/door/replication/ReplicationNotificationDispatcher.kt
+++ door-runtime/src/commonMain/kotlin/com/ustadmobile/door/replication/ReplicationNotificationDispatcher.kt
@@ -2,35 +2,6 @@ package com.ustadmobile.door.replication
 
 import com.ustadmobile.door.TableChangeListener
 
-/*
-
-1. Generated DbName_ReplicatedEntityChangeListener will listen for changes on the repository. Where a Replicated Entity
-is changed, it will run the annotated functions (e.g that insert into the tracker table) and trigger handleTableChanged
-for the tracker table.
-
-2. The replicationnotificationdispatcher will be accessible through the DI. It will run queries that determine which
-nodes have been affected. It will then fire ReplicationPendingEvent
-
-3. Those listening for ReplicationPendingEvent (e.g. Server Sent Event endpoint and SubscriptionManager) will act upon it.
-SubscriptionManager update its status (leading to a sendPendingReplicationCall) and a server sent event endpoint will
-sent an SSE.
-
- */
-/**
- *
- *
- *
- * This class listens for newly generated replications using a TableChangeListener. It will be stored in
- * the DI. The generated DbName_ReplicatedEntityChangeListener will run annotated functions, and it will then trigger
- *
- *
- * It will use queries to determine the affected nodes. It will be made accessible in the DI, so that it can be used
- * by a ReplicationSubscriptionManager or a Server Sent Event http endpoint.
- *
- * It will then fire ReplicationNotificationEvent
- */
-
-
 /**
  * 1) ReplicationNotificationDispatcher will listen to the database for table invalidations
  * 2) ReplicationNotificationDispatcher calls the generated ReplicationRunOnChangeRunner to run any queries that are annotated @ReplicationRunOnChange
