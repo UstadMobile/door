9a92b033cd8f8fb1c6c09bdcf5f8bf660f6818b5
kileha3 <lkileha@gmail.com>
2022-01-03 21:52:03 +0300

    Finalized test setup and bumped version

--- door-testdb/src/jsTest/kotlin/TestDbBuilder.kt
+++ door-testdb/src/jsTest/kotlin/TestDbBuilder.kt
@@ -6,25 +6,28 @@ import db2.ExampleDatabase2_JdbcKt
 import db2.ExampleEntity2
 import kotlinx.browser.window
 import kotlinx.coroutines.*
-import org.w3c.dom.Worker
 import org.w3c.dom.url.URL
 import org.w3c.files.Blob
 import kotlin.js.Promise
 import kotlin.test.*
 
 class TestDbBuilder {
-     lateinit var exampleDb2: ExampleDatabase2
 
-     //We can not use @BeforeTest here, it is async call test will run before it finishes setting up.
+     private lateinit var exampleDb2: ExampleDatabase2
+
+     //We can not use @BeforeTest here, it is async call test will run before it finishes the setting up.
      private suspend fun openClearDb() {
-        val res = (window.fetch("https://endlessfame.co.tz/worker.sql-asm.js") as Promise<dynamic>).await()
+        val res = (window.fetch("""
+            https://raw.githubusercontent.com/UstadMobile/door/dev-js-2/app-testdb/src/main/resources/worker.sql-asm.js
+        """.trimIndent()) as Promise<dynamic>).await()
         val data = (res.blob() as Promise<dynamic>).await()
         val workerBlobUrl = URL.createObjectURL(data as Blob)
         val builderOptions = DatabaseBuilderOptions(
             ExampleDatabase2::class,
             ExampleDatabase2_JdbcKt::class, "jsDb1",workerBlobUrl)
         exampleDb2 = DatabaseBuilder.databaseBuilder<ExampleDatabase2>(builderOptions).build()
-        //exampleDb2?.clearAllTables()
+         //TODO Still running synchronously, we need async for this
+        //exampleDb2.clearAllTables()
     }
 
     @Test
@@ -52,10 +55,11 @@ class TestDbBuilder {
 
     @Test
     fun givenEntryInserted_whenSingleValueQueried_shouldBeEqual() = GlobalScope.promise{
+        openClearDb()
         val entityToInsert = ExampleEntity2(0, "Bob" + systemTimeInMillis(),
             50)
         entityToInsert.uid = exampleDb2.exampleDao2().insertAsyncAndGiveId(entityToInsert)
-        assertEquals("++${entityToInsert.name}", exampleDb2.exampleDao2().findNameByUidAsync(entityToInsert.uid),
+        assertEquals(entityToInsert.name, exampleDb2.exampleDao2().findNameByUidAsync(entityToInsert.uid),
             "Select single column method returns expected value")
     }
 }
