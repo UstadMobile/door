b4059bf2f8986ec39a65b5661adee1b0f7929b2a
Mike Dawson <mike@ustadmobile.com>
2022-01-12 17:05:30 +0400

    Fix postgres trigger generation issue caused by typo

--- door-compiler/src/main/kotlin/com/ustadmobile/lib/annotationprocessor/core/AbstractDbProcessor.kt
+++ door-compiler/src/main/kotlin/com/ustadmobile/lib/annotationprocessor/core/AbstractDbProcessor.kt
@@ -447,7 +447,7 @@ abstract class AbstractDbProcessor: AbstractProcessor() {
                 add("$sqlListVar += %S\n",
                     """
             CREATE TRIGGER ch_${params.opPrefix}_${replicateEntity.tableId}_trig 
-                   AFTER ${params.opName} ON RepEntity
+                   AFTER ${params.opName} ON ${replicateEntity.tableId}
                    FOR EACH ROW
                    EXECUTE PROCEDURE ch_${params.opPrefix}_${replicateEntity.tableId}_fn();
             """.minifySql())
