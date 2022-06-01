2f437177c3bbe4ac84fa7ebf5cf935092789340f
Mike Dawson <mike@ustadmobile.com>
2022-06-01 12:53:32 +0200

    Add system to handle recording mime type on DoorUri for Javascript.

--- door-runtime/src/jsMain/kotlin/com/ustadmobile/door/DoorUri.kt
+++ door-runtime/src/jsMain/kotlin/com/ustadmobile/door/DoorUri.kt
@@ -9,8 +9,10 @@ import org.w3c.files.Blob
  */
 actual class DoorUri(val uri: URL)  {
 
+    data class DoorUriProps(val uri: String, val fileName: String?, val mimeType: String?)
+
     actual suspend fun getFileName(context: Any): String {
-        return filenameMap[uri.toString()] ?: uri.pathname.substringAfterLast( "/")
+        return doorUriInfo[uri.toString()]?.fileName ?: uri.pathname.substringAfterLast( "/")
     }
 
     override fun toString() = uri.toString()
@@ -18,21 +20,27 @@ actual class DoorUri(val uri: URL)  {
     actual companion object {
         actual fun parse(uriString: String) = DoorUri(URL(uriString))
 
-        private val filenameMap = mutableMapOf<String, String>()
+        private val doorUriInfo = mutableMapOf<String, DoorUriProps>()
 
         /**
          * Important: This uses URL.createLocalUrl. When finished, it must be released using revokeLocalUri which
          * will call revokeLocalUrl. Failing to do so causes a memory leak.
          */
-        fun createLocalUri(blob: Blob, filename: String): DoorUri {
+        fun createLocalUri(blob: Blob, filename: String, mimeType: String? = null): DoorUri {
             val localUrl = URL.createObjectURL(blob)
-            filenameMap[localUrl] = filename
+            doorUriInfo[localUrl] = DoorUriProps(localUrl, filename, mimeType)
             return DoorUri(URL(localUrl))
         }
 
+        /**
+         * Semi-internal - this can be used to lookup previously recorded information about the mime type and filename
+         * of a uri that was created using file.toDoorUri
+         */
+        fun getDoorUriProps(uri: String)  = doorUriInfo[uri]
+
         fun revokeLocalUri(localUri: DoorUri) {
             URL.revokeObjectURL(localUri.toString())
-            filenameMap.remove(localUri.toString())
+            doorUriInfo.remove(localUri.toString())
         }
 
     }
--- door-runtime/src/jsMain/kotlin/com/ustadmobile/door/ext/FileExt.kt
+++ door-runtime/src/jsMain/kotlin/com/ustadmobile/door/ext/FileExt.kt
@@ -7,5 +7,5 @@ import org.w3c.files.File
  * CAUTION: This uses createLocalUri / createObjectUrl. It must be revoked when finished with to avoid memory leaks.
  */
 fun File.toDoorUri() : DoorUri {
-    return DoorUri.createLocalUri(this, this.name)
+    return DoorUri.createLocalUri(this, this.name, this.type)
 }
