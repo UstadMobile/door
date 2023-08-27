13b5781739aca2aba6c7f48550360d86d287e9a2
Mike Dawson <mike@ustadmobile.com>
2023-08-27 22:32:54 +0400

    Update documentation on offline first data layer.

--- door-runtime/src/commonMain/kotlin/com/ustadmobile/door/nodeevent/NodeEventMessage.kt
+++ door-runtime/src/commonMain/kotlin/com/ustadmobile/door/nodeevent/NodeEventMessage.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.Serializable
  * used to transmit multiple events at a time (e.g. one message for the events of a particular database transaction).
  *
  * Looks like:
- *
+ *  'what': 1, //WHAT_REPLICATION
  *  'replications': [
  *     {
  *       tableId: 42,
